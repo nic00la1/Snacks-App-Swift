85620bb28a7d38345b4620fa4c424e5a1a8491c3
Nicola Kaleta <nic00la.kaleta@gmail.com>
2024-02-06 21:22:31 +0100

    Product Card View ✅

--- Snacks App/Models/ProductModel.swift
+++ Snacks App/Models/ProductModel.swift
@@ -13,17 +13,18 @@ struct Product: Identifiable {
     var name: String
     var category: String
     var image: String
+    var color: Color
     var price: Int
 }
 
 // Sample Products
 var productList = [
-    Product(name: "Good Source", category: "Choco", image: "image 1", price: 8),
-    Product(name: "Unreal Muffins", category: "Choco", image: "image 2", price: 8),
-    Product(name: "Twister Chips", category: "Chips", image: "image 3", price: 8),
-    Product(name: "Crazy Chippers", category: "Chips", image: "image 4", price: 8),
-    Product(name: "Regular Nature", category: "Chips", image: "image 5", price: 8),
-    Product(name: "Dark Russet", category: "Chips", image: "image 6", price: 8),
-    Product(name: "Smiths Chips", category: "Chips", image: "image 7", price: 8),
-    Product(name: "Deep River", category: "Chips", image: "image 8", price: 8)
+    Product(name: "Good Source", category: "Choco", image: "image 1",color: .pink, price: 8),
+    Product(name: "Unreal Muffins", category: "Choco", image: "image 2",color: .yellow, price: 8),
+    Product(name: "Twister Chips", category: "Chips", image: "image 3", color: .red, price: 8),
+    Product(name: "Crazy Chippers", category: "Chips", image: "image 4",color: .green,price: 8),
+    Product(name: "Regular Nature", category: "Chips", image: "image 5", color: .blue, price: 8),
+    Product(name: "Dark Russet", category: "Chips", image: "image 6",color: .brown ,price: 8),
+    Product(name: "Smiths Chips", category: "Chips", image: "image 7",color: .orange,price: 8),
+    Product(name: "Deep River", category: "Chips", image: "image 8", color: .purple,price: 8)
 ]
--- Snacks App/Views/Home.swift
+++ Snacks App/Views/Home.swift
@@ -46,6 +46,15 @@ struct Home: View {
                 }
                 .padding(.horizontal, 30)
                 .padding(.vertical, 15)
+                
+                // Product List
+                ScrollView(.horizontal, showsIndicators: false){
+                    HStack {
+                        ForEach(productList, id: \.id) { item in
+                            ProductCard(product: item)
+                        }
+                    }
+                }
             }
         }
     }
@@ -86,4 +95,58 @@ struct Home: View {
 
 #Preview {
     Home()
+}
+
+// Product Card View
+struct ProductCard: View {
+    var product: Product
+    
+    var body: some View {
+        ZStack {
+            VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, content: {
+                Text("\(product.name)")
+                    .font(.system(size: 36, weight: .semibold))
+                
+                Text("\(product.category)")
+                    .font(.callout)
+                    .padding()
+                    .background(.white.opacity(0.5))
+                    .clipShape(Capsule())
+                
+                Spacer()
+                
+                HStack {
+                    Text("$\(product.price).0")
+                        .font(.system(size: 24, weight: .semibold))
+                    
+                    Spacer()
+                    
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "basket")
+                            .imageScale(.large)
+                            .frame(width: 90, height: 68)
+                            .background(.black)
+                            .clipShape(Capsule())
+                            .foregroundStyle(.white)
+                    }
+                    
+                }
+                .padding()
+                .padding(.leading)
+                .frame(maxWidth: .infinity)
+                .frame(height: 80)
+                .background(.white.opacity(0.5))
+                .clipShape(Capsule())
+                
+                
+            })
+        }
+        .padding(30)
+        .frame(width: 336, height: 422)
+        .background(product.color.opacity(0.13))
+        .clipShape(.rect(cornerRadius: 57))
+        .padding(.leading, 20)
+    }
 }
