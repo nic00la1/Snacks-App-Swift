92c617f5d76648cee754c398e0ca995c51f0d6fc
Nicola Kaleta <nic00la.kaleta@gmail.com>
2024-02-07 16:56:55 +0100

    Zmiany po kliknięciu produktu w koszyku przenosi do make payment

--- Snacks App/ContentView.swift
+++ Snacks App/ContentView.swift
@@ -12,45 +12,51 @@ struct ContentView: View {
     
     var body: some View {
         
-        VStack(alignment: .leading) {
-            Home()
-                .environmentObject(cartManager)
-            
-            if cartManager.products.count > 0 {
-                HStack(spacing: 30) {
-                    Text("\(cartManager.products.count)")
-                        .padding()
-                        .background(.yellow)
-                        .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
-                        .foregroundStyle(.black)
-                    
-                    VStack(alignment: .leading) {
-                        Text("Cart")
-                            .font(.system(size: 26, weight: .semibold))
-                        
-                        Text("\(cartManager.products.count) Items")
-                            .font(.system(size: 18))
-                    }
-                    Spacer()
-                    
-                    ForEach(cartManager.products, id: \.name) { product in
-                        Image(product.image)
-                            .resizable()
-                            .scaledToFit()
-                            .padding(8)
-                            .frame(width: 60, height: 60)
-                            .background(.white)
-                            .clipShape(Circle())
-                            .padding(.leading, -60)
-                        
+        NavigationStack {
+            ZStack(alignment: .bottom) {
+                Home()
+                    .environmentObject(cartManager)
+                
+                if cartManager.products.count > 0 {
+                    NavigationLink(destination: CartView()) {
+                        HStack(spacing: 30) {
+                            Text("\(cartManager.products.count)")
+                                .padding()
+                                .background(.yellow)
+                                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                                .foregroundStyle(.black)
+                            
+                            VStack(alignment: .leading) {
+                                Text("Cart")
+                                    .font(.system(size: 26, weight: .semibold))
+                                
+                                Text("\(cartManager.products.count) Items")
+                                    .font(.system(size: 18))
+                            }
+                            Spacer()
+                            
+                            ForEach(cartManager.products, id: \.name) { product in
+                                Image(product.image)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .padding(8)
+                                    .frame(width: 60, height: 60)
+                                    .background(.white)
+                                    .clipShape(Circle())
+                                    .padding(.leading, -60)
+                                
+                            }
+                        }
+                        .padding(30)
+                        .frame(width: .infinity, height: 120)
+                        .background(.black)
+                        .clipShape(.rect(topLeadingRadius: 60, topTrailingRadius: 60))
+                        .foregroundStyle(.white)
+                        .edgesIgnoringSafeArea(.bottom)
                     }
                 }
-                .padding(30)
-                .frame(width: .infinity, height: 80)
-                .background(.black)
-                .foregroundStyle(.white)
             }
-            
+            .edgesIgnoringSafeArea(.bottom)
         }
     }
 }
